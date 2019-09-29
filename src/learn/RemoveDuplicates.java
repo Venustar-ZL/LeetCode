31e439e940d4e1f236c76c1c95431402e02673e3
zl <1577495441@qq.com>
2019-09-29 10:24:07 +0800

    删除排序数组中的重复项

---
+++ src/learn/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package learn;
+
+/**
+ * 删除排序数组中的重复项
+ */
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+}
