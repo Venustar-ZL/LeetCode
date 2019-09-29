bdd04ea4228a04afb6a226c8d7c9c78d16669508
zl <1577495441@qq.com>
2019-09-29 09:46:35 +0800

    将两个有序链表合并为一个新的有序链表并返回。

---
+++ src/learn/MergeTwoLists.java
@@ -0,0 +1,27 @@
+package learn;
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。
+ * 新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 == null ? l2 : l1;
+        return head.next;
+    }
+}
