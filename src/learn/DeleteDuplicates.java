cbef4cacb19234632771ef3b3386bddf053760f4
zl <1577495441@qq.com>
2019-10-09 16:27:58 +0800

    删除排序链表中的重复元素

---
+++ src/learn/DeleteDuplicates.java
@@ -0,0 +1,28 @@
+package learn;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 删除排序链表中的重复元素
+ */
+public class DeleteDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode list = head;
+        while (list.next != null) {
+            if (list.val == list.next.val) {
+                list.next = list.next.next;
+            }
+            else {
+                list = list.next;
+            }
+        }
+        return head;
+    }
+
+}
